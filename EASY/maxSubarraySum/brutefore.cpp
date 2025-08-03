eccc9b60291b1fbd19ca441ab5592c5993747c2c
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-08-03 11:57:23 +0530

    Create brutefore.cpp

---
+++ EASY/maxSubarraySum/brutefore.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void maxSubarraySum(int *arr,int n){
+    int maxsum = 0;
+    for(int start = 0; start < n; start++){
+        for(int end = start; end < n; end++){
+            int sum = 0;
+            for(int i = start; i <= end; i++){
+                sum += arr[i];
+            }
+            cout<<sum<<',';
+            maxsum = max(maxsum,sum);
+        }
+        cout<<endl;
+    }
+    cout<<"Maximum sum is :" << maxsum<<endl;
+}
+int main(){
+    int arr[6] = {2,-3,6,-5,4,2};
+    int n = 6;
+    maxSubarraySum(arr,n);
+    return 0;
+}
